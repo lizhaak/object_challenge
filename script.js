67189951331c694e540694e087385691b03c34f3
Liz Haakenson <lhaakens3@gmail.com>
2016-07-28 09:10:54 -0500

    created a state constructor to create state objects

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+function State(state, revenue, locationCount) {
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+    return revenue / locationCount;
+  }
+}
 
 // object instances
-
+var il = new State("IL", 5000, 12);
+var mn = new State("MN", 300, 3);
+var nv = new State("NV", 25000, 1);
 // push object instances to locations array
+locations.push(il);
+locations.push(mn);
+locations.push(nv);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
